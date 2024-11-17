3acb88e08b1148bea94aa3f402b0a49b3b3fc54a
Evin Capan <145271800+Evin-C@users.noreply.github.com>
2024-11-17 11:32:59 +0100

    added more description, organized the code a bit

--- do_mapping_convert.sh
+++ do_mapping_convert.sh
@@ -41,11 +41,15 @@ apptainer exec --bind /data/ /containers/apptainer/hisat2_samtools_408dfd02f175c
   bash -c "hisat2 -p 12 -x ${genome_index} -1 ${input_dir}/${R1_file} -2 ${input_dir}/${R2_file} \
   2> ${output_dir}/${R1_file%_R1.fastq.gz}_hisat2_summary.log | \
   samtools view -hbS -" > ${output_dir}/${R1_file%_R1.fastq.gz}_mappedReads.bam
-# -p 12 tells HISAT2 to use 12 threads (or CPU cores)
-# -x specifies the genome index to use
-# -1 and -2 specify the paired-end read files (R1 and R2)
-# -h displays help (optional)
-# -b tells samtools to convert the input into BAM format
-# -S indicates that the input file is in SAM format
+# --bind /data/ makes the /data/ directory accessible inside the container.
+# bash -c runs the following command (specified by -c) in a shell (bash) inside the container.
+# -p 12 tells HISAT2 to use 12 threads (or CPU cores).
+# -x specifies the genome index to use.
+# -1 and -2 specify the paired-end read files (R1 and R2).
+# -h displays help (optional).
+# -b tells samtools to convert the input into BAM format.
+# -S indicates that the input file is in SAM format.
 # - (hyphen) signifies that samtools should read from standard input (stdin).
 #   In this case, the samtools command is receiving its input from the pipe (|).
+# 2> ${output_dir}/${R1_file%_R1.fastq.gz}_hisat2_summary.log: Redirects HISAT2's error messages and alignment statistics
+# to a log file. The log file contains information such as alignment rates, concordant/discordant alignments and multimapping reads.
