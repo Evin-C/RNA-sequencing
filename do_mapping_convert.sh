eee3e0e553247d8a5fa3cc73bf0f68145390a5a6
Evin Capan <145271800+Evin-C@users.noreply.github.com>
2024-11-17 02:44:07 +0100

    changed array to 0-11 after testing, increased memory,cpu and time assignments

--- do_mapping_convert.sh
+++ do_mapping_convert.sh
@@ -4,11 +4,11 @@
 #SBATCH --output=hisat2_mapping_%A_%a.out        # Standard output log with job array index
 #SBATCH --error=hisat2_mapping_%A_%a.err         # Standard error log with job array index
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8                        # Number of CPUs per task is 8 (same as for HISAT2)
-#SBATCH --time=14:00:00                          # 14 hours runtime
-#SBATCH --mem=18GB                               # 18GB memory
+#SBATCH --cpus-per-task=12                       # Number of CPUs per task is 12 (same as for HISAT2)
+#SBATCH --time=15:00:00                          # 15 hours runtime
+#SBATCH --mem=32GB                               # 32GB memory
 #SBATCH --partition=pibu_el8
-#SBATCH --array=0-0                             # Array range for 12 pairs of fastq files (0-11 for 12 files)
+#SBATCH --array=0-11                             # Array range for 12 pairs of fastq files (0-11 for 12 files)
 
 USER="ecapan"                                    # Define variable USER for general use
 
@@ -38,10 +38,10 @@ R2_file="${R1_file/R1/R2}"    # Replace R1 with R2 for the mate pair
 
 # Run HISAT2 and pipe the output directly to Samtools to convert SAM to BAM inside the Apptainer container:
 apptainer exec --bind /data/ /containers/apptainer/hisat2_samtools_408dfd02f175cd88.sif \
-  bash -c "hisat2 -p 8 -x ${genome_index} -1 ${input_dir}/${R1_file} -2 ${input_dir}/${R2_file} \
+  bash -c "hisat2 -p 12 -x ${genome_index} -1 ${input_dir}/${R1_file} -2 ${input_dir}/${R2_file} \
   2> ${output_dir}/${R1_file%_R1.fastq.gz}_hisat2_summary.log | \
   samtools view -hbS -" > ${output_dir}/${R1_file%_R1.fastq.gz}_mappedReads.bam
-# -p 8 tells HISAT2 to use 8 threads (or CPU cores)
+# -p 12 tells HISAT2 to use 12 threads (or CPU cores)
 # -x specifies the genome index to use
 # -1 and -2 specify the paired-end read files (R1 and R2)
 # -h displays help (optional)
