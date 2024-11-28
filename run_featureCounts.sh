d26a33b647fe8bf7cbdaafaf7aa1aa2131efd328
Evin Capan <145271800+Evin-C@users.noreply.github.com>
2024-11-28 15:51:03 +0100

    changed memory allocation

--- run_featureCounts.sh
+++ run_featureCounts.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=featurecounts_%j.out    # Standard output log
 #SBATCH --error=featurecounts_%j.err     # Standard error log
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4                # Use 4 CPUs
-#SBATCH --time=15:00:00                  # 15 hours runtime
-#SBATCH --mem=10GB                       # Memory allocation
+#SBATCH --cpus-per-task=12               # Use 12 CPUs
+#SBATCH --time=23:59:00                  # 23h59min runtime
+#SBATCH --mem=12GB                       # 12GB Memory allocation
 #SBATCH --partition=pibu_el8
 
 USER="ecapan"                            # Define USER variable for general use
@@ -27,12 +27,12 @@ counts_output="${output_dir}/gene_counts.txt"
 
 # Run featureCounts:
 apptainer exec --bind /data/ /containers/apptainer/subread_2.0.1--hed695b0_0.sif \
-  featureCounts -T 4 -p -t exon -g gene_id \
+  featureCounts -T 12 -p -t exon -g gene_id \
   -a ${annotation_file} \
   -o ${counts_output} \
   ${bam_files[@]}
 
-# -T 4: Use 4 threads (matches SLURM settings).
+# -T 12: Use 12 threads (matches SLURM settings).
 # -p: Enable paired-end mode (treat input BAM files as paired-end reads).
 # -t exon: Count reads mapped to exon features.
 # -g gene_id: Group reads by gene ID (as specified in the GTF file's "gene_id" attribute).
